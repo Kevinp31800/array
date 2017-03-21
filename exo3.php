5ad7ca10762355bdce09819ba0e4d67ec73877fc
kevin <kevinpuyol@live.fr>
2017-03-21 16:00:44 +0100

    exo3

--- exo3.php
+++ exo3.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>exo1</title>
+	<title>exo3</title>
 </head>
 <body>
 	<?php 
 	$mois = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
-	echo $mois;
+	echo $mois[5];
 	?>
 </body>
 </html>
